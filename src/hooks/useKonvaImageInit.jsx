9792fc6e88c7663ec9db1a633228450b21a47cca
patrice902 <patricereddick902@yahoo.com>
2022-10-31 17:15:53 -0400

    debugging

--- src/hooks/useKonvaImageInit.jsx
+++ src/hooks/useKonvaImageInit.jsx
@@ -65,7 +65,7 @@ export const useKonvaImageInit = ({
       clearCache(imageshapeRef.current);
       const pixelRatio = getPixelRatio(imageshapeRef.current, imageRef.current);
       imageshapeRef.current.cache({
-        pixelRatio: isDesktop ? pixelRatio : Math.min(pixelRatio * 0.5, 0.5),
+        pixelRatio: isDesktop ? pixelRatio : Math.min(pixelRatio * 0.3, 0.3),
         imageSmoothingEnabled: true,
       });
     }
